a37124f37669db9522e95136ef910dac00c18b1f
Marc Font <36164126+Marketen@users.noreply.github.com>
2023-11-17 15:44:20 +0100

    Swap smooth fee from rewards to total subscribers card (#32)
    swap smooth fee from rewards to total subscribers card

--- src/components/cards/SummaryCard/TotalSubscribersCard/index.tsx
+++ src/components/cards/SummaryCard/TotalSubscribersCard/index.tsx
@@ -1,18 +1,33 @@
 import { SummaryCard, SummaryCardProps } from '..'
 import { BsFillPersonFill } from 'react-icons/bs'
+import { toFixedNoTrailingZeros } from '@/utils/decimals'
 
 interface TotalSubscribersCardProps
   extends Pick<SummaryCardProps, 'isError' | 'isLoading'> {
   subscribers: number | undefined
+  poolFeesPercent: number | undefined
 }
 
 export function TotalSubscribersCard({
   isError,
   isLoading,
+  poolFeesPercent,
   subscribers,
 }: TotalSubscribersCardProps) {
+  let adjustedPoolFeesPercent = poolFeesPercent
+
+  if (adjustedPoolFeesPercent !== undefined) {
+    // poolFeesPercent is multiplied x 100 in the backend, so 1000 = 10%, this is
+    // because the oracle doesnt want to deal with decimals.
+    // We assume poolFeesPercent will never be 0.
+    adjustedPoolFeesPercent /= 100
+  } else {
+    adjustedPoolFeesPercent = NaN
+  }
   return (
     <SummaryCard
+      bottomLeftText="Smooth Fee"
+      bottomRightText={`${toFixedNoTrailingZeros(adjustedPoolFeesPercent || NaN, 4)}%`}
       isError={isError}
       isLoading={isLoading}
       title="Total Subscribers">
