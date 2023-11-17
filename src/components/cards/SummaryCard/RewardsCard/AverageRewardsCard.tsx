31e9326d14e423fe25a1fdc1b59ef098121d37b6
Marketen <marcfont12@gmail.com>
2023-11-17 16:55:59 +0100

    substract fee from validator rewards

--- src/components/cards/SummaryCard/RewardsCard/AverageRewardsCard.tsx
+++ src/components/cards/SummaryCard/RewardsCard/AverageRewardsCard.tsx
@@ -1,20 +1,42 @@
-import { AverageCard } from '.'
+import { BigNumber } from 'ethers';
+import { AverageCard } from '.';
 
 interface AverageRewardsCardProps {
   isLoading: boolean
   isError: boolean
   rewardsPerValidatorPer30daysWei: string | undefined
+  poolFeesPercent: number | undefined;
 }
 
 export function AverageRewardsCard({
   isLoading,
   isError,
   rewardsPerValidatorPer30daysWei,
+  poolFeesPercent
 }: AverageRewardsCardProps) {
+
+  let adjustedPoolFeesPercent = poolFeesPercent;
+
+  let adjustedRewardsWei = rewardsPerValidatorPer30daysWei;
+
+  if (adjustedPoolFeesPercent !== undefined && rewardsPerValidatorPer30daysWei !== undefined) {
+    adjustedPoolFeesPercent /= 100; // Convert to a decimal
+
+    // Convert rewardsPerValidatorPer30daysWei to a BigNumber
+    const rewardsWeiBigNumber = BigNumber.from(rewardsPerValidatorPer30daysWei);
+
+    // Calculate the fee amount (multiply rewards by the fee percent and divide by 100)
+    const feeAmount = rewardsWeiBigNumber.mul(adjustedPoolFeesPercent).div(100);
+
+    // Subtract the fee amount from the original rewards
+    adjustedRewardsWei = rewardsWeiBigNumber.sub(feeAmount).toString();
+  } else {
+    adjustedRewardsWei = undefined;
+  }
 
   return (
     <AverageCard
-      ethRewardWei={rewardsPerValidatorPer30daysWei}
+      ethRewardWei={adjustedRewardsWei}
       isError={isError}
       isLoading={isLoading}
       title="Validator's Monthly Rewards"
