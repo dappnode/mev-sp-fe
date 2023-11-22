99b14b70f7b3648bd7990dfa698641480cbebf9d
Marketen <marcfont12@gmail.com>
2023-11-22 11:18:53 +0100

    fix donation table tooltips

--- src/components/tables/LatestDonationsTable/config.ts
+++ src/components/tables/LatestDonationsTable/config.ts
@@ -3,6 +3,6 @@ export const PAGE_SIZE = 10
 export const headerTooltip = {
   txHash: 'Transaction hash',
   sender: 'Sender address',
-  reward: 'Reward amount',
-  blockNumber: 'Block number',
+  reward: 'Donation amount',
+  blockNumber: 'Block number where the donation was made',
 }
