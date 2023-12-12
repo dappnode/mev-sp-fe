936c6e502bdf6c428e1e86229f2c89baf3427758
Ginessi <giselle.nessi@trili.tech>
2023-12-12 12:30:59 +0100

    updated footer link style

--- src/components/layout/Footer/index.tsx
+++ src/components/layout/Footer/index.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { IoLogoDiscord } from 'react-icons/io5'
 import { FaGithubSquare, FaLinkedin, FaTwitterSquare } from 'react-icons/fa'
+import { FiExternalLink } from 'react-icons/fi'
 import { MAIN_SITE_URL, SOCIALS } from '@/utils/config'
 
 const socialIcons = {
@@ -14,21 +15,28 @@ const socialIcons = {
 export function Footer() {
   return (
     <footer className="flex h-36 flex-col items-center justify-center gap-y-4 border-t bg-white px-4 sm:h-20 sm:flex-row sm:justify-between md:px-12">
-      <Link
-        className="flex flex-col items-center gap-y-4 sm:flex-row"
-        href={MAIN_SITE_URL}
-        rel="noopener noreferrer"
-        target="_blank">
-        <Image
-          alt="Dappnode logo"
-          height={26}
-          src="/images/dappnode-logo.svg"
-          width={26}
-        />
-        <h4 className="text-center text-xs sm:mx-3 sm:text-left">
-          Dappnode | Decentralized P2P Networking | © 2023
+      <div className="flex flex-col items-center justify-between sm:flex-row">
+        {/* Link with logo and text */}
+        <div className="flex flex-col items-center gap-y-4 hover:underline sm:flex-row">
+          <Link href={MAIN_SITE_URL} rel="noopener noreferrer" target="_blank">
+            <Image
+              alt="Dappnode logo"
+              height={26}
+              src="/images/dappnode-logo.svg"
+              width={26}
+            />
+          </Link>
+          <h4 className="ml-4 mt-2 flex items-center justify-center text-xs sm:text-left">
+            Dappnode <FiExternalLink className="ml-1" />
+          </h4>
+        </div>
+
+        {/* Right side text */}
+        <h4 className="mt-2 text-center text-xs sm:mx-3 sm:text-left">
+          | Decentralized P2P Networking | © 2023
         </h4>
-      </Link>
+      </div>
+
       <nav className="flex gap-x-5">
         {SOCIALS.map(({ name, path }) => {
           const Icon = socialIcons[name]
