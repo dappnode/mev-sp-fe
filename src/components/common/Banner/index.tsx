9c74239e2bc27c243a21ca990d0076afbb8f133c
Ginessi <giselle.nessi@trili.tech>
2024-01-03 16:05:26 +0100

    updated text in banner

--- src/components/common/Banner/index.tsx
+++ src/components/common/Banner/index.tsx
@@ -1,27 +1,36 @@
 export default function Banner() {
   return (
-    <div className="flex items-center gap-x-6 border-b border-purple-500 bg-white px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
+    <div className="flex items-center border-b border-purple-500 bg-white py-2.5 sm:px-3.5 sm:before:flex-1">
       <p className="rotating-text text-sm leading-6 text-black">
-        <div>
-          <strong className="font-semibold">🎁 Special NY Gift 🎉</strong>
-          <svg
-            aria-hidden="true"
-            className="mx-2 inline h-0.5 w-0.5 fill-current"
-            viewBox="0 0 2 2">
-            <circle cx={1} cy={1} r={1} />
-          </svg>
-          5 Ether donation to Smooth by January 11th! 💰 JOIN NOW TO GET
-          IT!&nbsp;
-          <strong className="ml-1 font-semibold">🎁 Special NY Gift 🎉</strong>
-          <svg
-            aria-hidden="true"
-            className="mx-2 inline h-0.5 w-0.5 fill-current"
-            viewBox="0 0 2 2">
-            <circle cx={1} cy={1} r={1} />
-          </svg>
-          5 Ether donation to Smooth by January 11th! 💰 JOIN NOW TO GET
-          IT!&nbsp;
-          <strong className="ml-1 font-semibold">🎁 Special NY Gift 🎉</strong>
+        <div className="flex space-x-2">
+          <strong className="mx-2 font-semibold ">🎁 Special NY Gift 🎉</strong>
+          <span>
+            5 Ether donation to Smooth by January 11th! 💰 JOIN NOW TO GET IT!
+          </span>
+          <strong className="mx-2 font-semibold">🎁 Special NY Gift 🎉</strong>
+          <span>
+            5 Ether donation to Smooth by January 11th! 💰 JOIN NOW TO GET IT!
+          </span>
+        </div>
+        <div className="flex space-x-2">
+          <strong className="mx-2 font-semibold">🎁 Special NY Gift 🎉</strong>
+          <span>
+            5 Ether donation to Smooth by January 11th! 💰 JOIN NOW TO GET IT!
+          </span>
+          <strong className="mx-2 font-semibold">🎁 Special NY Gift 🎉</strong>
+          <span>
+            5 Ether donation to Smooth by January 11th! 💰 JOIN NOW TO GET IT!
+          </span>
+        </div>
+        <div className="flex space-x-2">
+          <strong className="mx-2 font-semibold">🎁 Special NY Gift 🎉</strong>
+          <span>
+            5 Ether donation to Smooth by January 11th! 💰 JOIN NOW TO GET IT!
+          </span>
+          <strong className="mx-2 font-semibold">🎁 Special NY Gift 🎉</strong>
+          <span>
+            5 Ether donation to Smooth by January 11th! 💰 JOIN NOW TO GET IT!
+          </span>
         </div>
       </p>
     </div>
