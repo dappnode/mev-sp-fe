ac19a8be4dd5c81ec888f1c44ea3435305df1c75
Marc Font <36164126+Marketen@users.noreply.github.com>
2024-01-09 16:12:06 +0100

    -Add overflow-hidden to banner in order to not expand page's width (#87) (#88)
    Co-authored-by: Mateu Miralles <52827122+mateumiralles@users.noreply.github.com>

--- src/components/common/Banner/index.tsx
+++ src/components/common/Banner/index.tsx
@@ -1,6 +1,6 @@
 export default function Banner() {
   return (
-    <div className="flex items-center border-b border-purple-500 bg-white py-2.5 sm:px-3.5 sm:before:flex-1">
+    <div className="flex items-center border-b border-purple-500 bg-white py-2.5 sm:px-3.5 sm:before:flex-1 overflow-hidden">
       <p className="rotating-text text-sm leading-6 text-black">
         <div className="flex space-x-2">
           <strong className="mx-2 font-semibold ">🎁 Special NY Gift 🎉</strong>
