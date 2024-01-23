a8814cb7278ef1c247c0b5cd3b86567d055adb00
Ginessi <giselle.nessi@trili.tech>
2024-01-23 11:05:58 +0100

    added google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,27 @@
+/* eslint-disable react/no-danger */
+/* eslint-disable @next/next/next-script-for-ga */
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-C7R9Y6T56G"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-C7R9Y6T56G');
+              `,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
