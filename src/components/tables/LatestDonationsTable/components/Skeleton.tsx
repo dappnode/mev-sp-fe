f7fb0be283d5b3bbc35dd27c34363546fd40c647
Mateu Miralles <52827122+mateumiralles@users.noreply.github.com>
2024-02-28 14:54:07 +0100

    Implementing dark mode in dashboard tables skeleton (#120)

--- src/components/tables/LatestDonationsTable/components/Skeleton.tsx
+++ src/components/tables/LatestDonationsTable/components/Skeleton.tsx
@@ -6,8 +6,8 @@ interface SkeletonProps {
 
 export function Skeleton({ title }: SkeletonProps) {
   return (
-    <div className="h-[650px] w-full overflow-hidden rounded-lg bg-white">
-      <h3 className="p-6 text-2xl font-bold leading-8 text-DAppDeep">
+    <div className="h-[650px] w-full overflow-hidden rounded-lg bg-white  dark:bg-DAppDarkSurface/200 ">
+      <h3 className="p-6 text-2xl font-bold leading-8 text-DAppDeep dark:text-DAppDarkText">
         {title}
       </h3>
       {[...Array(7)].map(() => (
@@ -15,12 +15,12 @@ export function Skeleton({ title }: SkeletonProps) {
           key={uuidv4()}
           className="mb-8 flex w-full items-center justify-between px-6">
           <div>
-            <div className="h-5 w-[500px] animate-pulse rounded-sm bg-DAppLight" />
-            <div className="mt-3 h-4 w-[300px] animate-pulse rounded-sm bg-DAppLight" />
+            <div className="h-5 w-[500px] animate-pulse rounded-sm bg-DAppLight dark:bg-DAppDarkSurface/300" />
+            <div className="mt-3 h-4 w-[300px] animate-pulse rounded-sm bg-DAppLight dark:bg-DAppDarkSurface/300" />
           </div>
           <div>
-            <div className="h-5 w-[110px] animate-pulse rounded-sm bg-DAppLight" />
-            <div className="mt-3 mr-0 ml-auto h-4 w-[60px] animate-pulse rounded-sm bg-DAppLight" />
+            <div className="h-5 w-[110px] animate-pulse rounded-sm bg-DAppLight dark:bg-DAppDarkSurface/300" />
+            <div className="ml-auto mr-0 mt-3 h-4 w-[60px] animate-pulse rounded-sm bg-DAppLight dark:bg-DAppDarkSurface/300" />
           </div>
         </div>
       ))}
