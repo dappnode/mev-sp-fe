44e5f5e2bcb673cdae45c68b3d7a2687a68625b7
Marketen <marcfont12@gmail.com>
2024-04-05 13:03:05 +0200

    add smooth address from config

--- src/components/home/HowToSubscribe.tsx
+++ src/components/home/HowToSubscribe.tsx
@@ -2,8 +2,7 @@
 import { CheckIcon } from '@heroicons/react/20/solid';
 import Link from 'next/link';
 import { useState } from 'react';
-
-const smoothAddress = '0xAdFb8D27671F14f297eE94135e266aAFf8752e35';
+import { SMOOTHING_POOL_ADDRESS } from '@/utils/config';
 
 export default function HowToSubscribe() {
     const [copied, setCopied] = useState(false);
@@ -11,7 +10,7 @@ export default function HowToSubscribe() {
     const handleCopyAddress = (e: React.MouseEvent<HTMLSpanElement, MouseEvent> | React.KeyboardEvent<HTMLSpanElement>): void => {
         e.preventDefault();
         if (e.type === 'click' || (e.type === 'keydown' && (e as React.KeyboardEvent<HTMLSpanElement>).key === 'Enter')) {
-            navigator.clipboard.writeText(smoothAddress);
+            navigator.clipboard.writeText(SMOOTHING_POOL_ADDRESS);
             setCopied(true);
             setTimeout(() => {
                 setCopied(false);
@@ -38,7 +37,7 @@ export default function HowToSubscribe() {
                 >
                     Change your validator&apos;s fee recipient to{' '}
                     <dl style={{ display: 'flex', justifyContent: 'space-between', cursor: 'pointer', textDecoration: 'underline' }}>
-                        <dt>{smoothAddress}</dt>
+                        <dt>{SMOOTHING_POOL_ADDRESS}</dt>
                         <dd>
                             <svg className="h-6 w-6" fill="none" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5A3.375 3.375 0 0 0 6.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0 0 15 2.25h-1.5a2.251 2.251 0 0 0-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 0 0-9-9Z" strokeLinecap="round" strokeLinejoin="round" />
