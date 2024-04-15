ec89cad924789df64b67fe5e4b3d9f7384db277f
Marketen <marcfont12@gmail.com>
2024-04-15 18:05:08 +0200

    fix legends

--- src/components/charts/AllBlocksSortedByRewardChart.tsx
+++ src/components/charts/AllBlocksSortedByRewardChart.tsx
@@ -1,39 +1,80 @@
-import React from 'react';
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
-import CustomTooltip from '@/components/common/ChartsTooltip';
-import styles from '@/styles/stats.module.css';
+import React from 'react'
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from 'recharts'
+import CustomTooltip from '@/components/common/ChartsTooltip'
+import styles from '@/styles/stats.module.css'
 
 interface AllBlocksSortedByRewardChartProps {
-    sortedBlocks: {
-        blockNumber: number;
-        rewardEth: number;
-    }[];
-    isLoading: boolean;
-    isError: boolean;
-    resolvedTheme?: string;
+  sortedBlocks: {
+    blockNumber: number
+    rewardEth: number
+  }[]
+  isLoading: boolean
+  isError: boolean
+  resolvedTheme?: string
 }
 
-function AllBlocksSortedByRewardChart({ sortedBlocks, isLoading, isError, resolvedTheme }: AllBlocksSortedByRewardChartProps) {
-    if (isLoading) return <div>Loading...</div>;
-    if (isError) return <div>There was an error loading this chart.</div>;
-    if (!sortedBlocks.length) return null;
+function AllBlocksSortedByRewardChart({
+  sortedBlocks,
+  isLoading,
+  isError,
+  resolvedTheme,
+}: AllBlocksSortedByRewardChartProps) {
+  if (isLoading) return <div>Loading...</div>
+  if (isError) return <div>There was an error loading this chart.</div>
+  if (!sortedBlocks.length) return null
 
-    const maxReward = Math.max(...sortedBlocks.map(block => block.rewardEth));
+  const maxReward = Math.max(...sortedBlocks.map((block) => block.rewardEth))
 
-    return (
-        <div>
-            <h2 className={styles.chartTitle}>All Blocks Sorted by MEV Reward</h2>
-            <ResponsiveContainer height={400} width="100%">
-                <BarChart data={sortedBlocks} layout="horizontal" margin={{ top: 20, right: 30, left: 20, bottom: 17 }}>
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <YAxis domain={[0, Math.ceil(maxReward)]} />
-                    <XAxis dataKey="blockNumber" tickLine={false} />
-                    <Tooltip content={<CustomTooltip {...{ resolvedTheme }} />} />
-                    <Bar dataKey="rewardEth" fill={resolvedTheme === 'dark' ? '#6B21A8' : '#C084FC'} isAnimationActive={false} name="ETH Reward" />
-                </BarChart>
-            </ResponsiveContainer>
-        </div>
-    );
+  return (
+    <div>
+      <h2 className={styles.chartTitle}>All Blocks Sorted by MEV Reward</h2>
+      <ResponsiveContainer height={400} width="100%">
+        <BarChart
+          data={sortedBlocks}
+          layout="horizontal"
+          margin={{ top: 20, right: 30, left: 20, bottom: 17 }}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <YAxis
+            domain={[0, Math.ceil(maxReward)]}
+            type="number"
+            label={{
+              value: `MEV Reward (ETH)`,
+              style: { textAnchor: 'middle' },
+              position: 'left',
+              angle: -90,
+              offset: -10,
+            }}
+          />
+          <XAxis
+            dataKey="blockNumber"
+            tick={{ fontSize: 0 }}
+            tickLine={false}
+            label={{
+              value: `Proposed blocks`,
+              style: { textAnchor: 'middle' },
+              position: 'bottom',
+              offset: 0,
+            }}
+          />
+          <Tooltip content={<CustomTooltip {...{ resolvedTheme }} />} />
+          <Bar
+            dataKey="rewardEth"
+            fill={resolvedTheme === 'dark' ? '#6B21A8' : '#C084FC'}
+            isAnimationActive={false}
+            name="ETH Reward"
+          />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  )
 }
 
-export default AllBlocksSortedByRewardChart;
+export default AllBlocksSortedByRewardChart
--- src/components/charts/RewardDistributionBucketChart.tsx
+++ src/components/charts/RewardDistributionBucketChart.tsx
@@ -1,78 +1,127 @@
-import React from 'react';
-import { ComposedChart, Bar, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-import CustomTooltip from '@/components/common/ChartsTooltip';
-import styles from '@/styles/stats.module.css';
-import { weiToEth } from '@/utils/web3'; 
-import { toFixedNoTrailingZeros } from '@/utils/decimals';
-import type { ProposedBlock } from '@/components/charts/types';
+import React from 'react'
+import {
+  ComposedChart,
+  Bar,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from 'recharts'
+import CustomTooltip from '@/components/common/ChartsTooltip'
+import styles from '@/styles/stats.module.css'
+import { weiToEth } from '@/utils/web3'
+import { toFixedNoTrailingZeros } from '@/utils/decimals'
+import type { ProposedBlock } from '@/components/charts/types'
 
 interface RewardDistributionBucketChartProps {
-    proposedBlocks?: ProposedBlock[];
-    isLoading: boolean;
-    isError: boolean;
-    resolvedTheme?: string;
+  proposedBlocks?: ProposedBlock[]
+  isLoading: boolean
+  isError: boolean
+  resolvedTheme?: string
 }
 
 function RewardDistributionBucketChart({
-    isError,
-    isLoading,
-    proposedBlocks,
-    resolvedTheme
+  isError,
+  isLoading,
+  proposedBlocks,
+  resolvedTheme,
 }: RewardDistributionBucketChartProps) {
-    if (isLoading) return <div>Loading...</div>;
-    if (isError) return <div>There was an error loading this chart.</div>;
-    if (!proposedBlocks) return null;
+  if (isLoading) return <div>Loading...</div>
+  if (isError) return <div>There was an error loading this chart.</div>
+  if (!proposedBlocks) return null
 
-    const rewardValueBuckets = [
-        { range: '> 10 ETH', min: 10, max: Infinity },
-        { range: '10 - 1 ETH', min: 1, max: 10 },
-        { range: '1 - 0.1 ETH', min: 0.1, max: 1 },
-        { range: '0.1 - 0.01 ETH', min: 0.01, max: 0.1 },
-        { range: '< 0.01 ETH', min: 0, max: 0.01 },
-    ];
+  const rewardValueBuckets = [
+    { range: '> 10 ETH', min: 10, max: Infinity },
+    { range: '10 - 1 ETH', min: 1, max: 10 },
+    { range: '1 - 0.1 ETH', min: 0.1, max: 1 },
+    { range: '0.1 - 0.01 ETH', min: 0.01, max: 0.1 },
+    { range: '< 0.01 ETH', min: 0, max: 0.01 },
+  ]
 
-    const bucketCounts = rewardValueBuckets.map(() => 0);
-    const bucketSums = rewardValueBuckets.map(() => 0);
+  const bucketCounts = rewardValueBuckets.map(() => 0)
+  const bucketSums = rewardValueBuckets.map(() => 0)
 
-    proposedBlocks.forEach(block => {
-        const rewardEth = weiToEth(block.rewardWei);
-        rewardValueBuckets.forEach((bucket, index) => {
-            if (rewardEth >= bucket.min && rewardEth < bucket.max) {
-                bucketCounts[index] += 1;
-                bucketSums[index] += rewardEth;
-            }
-        });
-    });
+  proposedBlocks.forEach((block) => {
+    const rewardEth = weiToEth(block.rewardWei)
+    rewardValueBuckets.forEach((bucket, index) => {
+      if (rewardEth >= bucket.min && rewardEth < bucket.max) {
+        bucketCounts[index] += 1
+        bucketSums[index] += rewardEth
+      }
+    })
+  })
 
-    const data = rewardValueBuckets.map((bucket, index) => ({
-        range: bucket.range,
-        blocks: bucketCounts[index],
-        sum: toFixedNoTrailingZeros(bucketSums[index], 4),
-    }));
-    const maxSum = Math.max(...bucketSums);
+  const data = rewardValueBuckets.map((bucket, index) => ({
+    range: bucket.range,
+    blocks: bucketCounts[index],
+    sum: toFixedNoTrailingZeros(bucketSums[index], 4),
+  }))
+  const maxSum = Math.max(...bucketSums)
 
-    return (
-        <div>
-            <h2 className={styles.chartTitle}>Proposed MEV Block Distribution by Ranges</h2>
-            <ResponsiveContainer height={350} width="100%">
-                <ComposedChart data={data} margin={{ bottom: 35, left: 5 }}>
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="range" />
-                    <YAxis yAxisId="left" />
-                    <YAxis
-                        yAxisId="right"
-                        orientation="right"
-                        domain={[0, Math.ceil(maxSum)]}
-                        label={{ value: 'Total ETH', angle: -90, position: 'right', offset: -10 }}
-                    />
-                    <Tooltip content={<CustomTooltip {...{ resolvedTheme }} />} />
-                    <Legend verticalAlign="top" height={20} />
-                    <Bar yAxisId="left" dataKey="blocks" fill={resolvedTheme === 'dark' ? '#6B21A8' : '#C084FC'} name="Amount of blocks" />
-                    <Line yAxisId="right" dataKey="sum" type="monotone" stroke="#FFB900" name="Total ETH" />
-                </ComposedChart>
-            </ResponsiveContainer>
-        </div>
-    );
+  return (
+    <div>
+      <h2 className={styles.chartTitle}>
+        Proposed MEV Block Distribution by Ranges
+      </h2>
+      <ResponsiveContainer height={350} width="100%">
+        <ComposedChart data={data} margin={{ bottom: 35, left: 5 }}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis
+            dataKey="range"
+            dy={5}
+            label={{
+              value: `MEV reward range`,
+              style: { textAnchor: 'middle' },
+              position: 'bottom',
+              offset: 10,
+            }}
+          />
+          <YAxis
+            yAxisId="left"
+            label={{
+              value: `Number of blocks`,
+              style: { textAnchor: 'middle' },
+              angle: -90,
+              position: 'left',
+              offset: 0,
+            }}
+          />
+          <YAxis
+            domain={[0, Math.ceil(maxSum)]}
+            orientation="right"
+            stroke="#FFB900"
+            tickFormatter={(value) => `${value} `}
+            yAxisId="right"
+            label={{
+              value: `Total ETH`,
+              style: { textAnchor: 'middle' },
+              angle: -90,
+              position: 'right',
+              offset: -10,
+            }}
+          />
+          <Tooltip content={<CustomTooltip {...{ resolvedTheme }} />} />
+          <Legend verticalAlign="top" height={40} />
+          <Bar
+            yAxisId="left"
+            dataKey="blocks"
+            fill={resolvedTheme === 'dark' ? '#6B21A8' : '#C084FC'}
+            name="Amount of blocks"
+          />
+          <Line
+            yAxisId="right"
+            dataKey="sum"
+            type="monotone"
+            stroke="#FFB900"
+            name="Total ETH"
+          />
+        </ComposedChart>
+      </ResponsiveContainer>
+    </div>
+  )
 }
 
-export default RewardDistributionBucketChart;
+export default RewardDistributionBucketChart
