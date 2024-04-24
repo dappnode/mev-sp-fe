78310779f3e26881a8fae3a226af705d08d5b0e1
Mateumiralles <mateumiralles714@gmail.com>
2024-04-24 16:25:39 +0200

    multiple UI corrections

--- src/components/home/Donate.tsx
+++ src/components/home/Donate.tsx
@@ -2,20 +2,25 @@ import { useAccount } from 'wagmi'
 import { DonateDialog } from '@/components/dialogs/DonateDialog'
 
 export default function Donate() {
-    const { isConnected } = useAccount()
+  const { isConnected } = useAccount()
 
-    return (
-        <div className="mt-32 rounded-3xl bg-gradient-to-br from-DAppPurple/900 to-DAppBlue text-center">
-            <div className="mx-auto max-w-7xl p-6 py-12 sm:py-16 lg:px-8">
-                <h2 className="mx-auto max-w-2xl text-3xl font-bold tracking-tight text-white sm:text-4xl">
-                    Donate to the Pool and Support Ethereum Decentralization
-                </h2>
-                <p className="mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-300">
-                    Contribute to the pool and increase solo staker&apos;s revenue. It&apos;s a great way of supporting the true decentralized node runners that keep Ethereum decentralized at scale.</p>
-                <div className="mt-8 flex w-[220px] items-center justify-center pt-10 lg:mx-0" style={{ margin: "auto" }}>
-                    {isConnected ? <DonateDialog /> : <w3m-button label="Donate now" />}
-                </div>
-            </div>
+  return (
+    <div className="mt-32 rounded-3xl bg-gradient-to-br from-DAppPurple/900 to-DAppBlue text-center transition duration-300 ease-in-out md:hover:scale-105">
+      <div className="mx-auto max-w-7xl p-6 py-12 sm:py-16 lg:px-8">
+        <h2 className="mx-auto max-w-2xl text-3xl font-bold tracking-tight text-white sm:text-4xl">
+          Donate to the Pool and Support Ethereum Decentralization
+        </h2>
+        <p className="mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-300">
+          Contribute to the pool and increase solo staker&apos;s revenue.
+          It&apos;s a great way of supporting the true decentralized node
+          runners that keep Ethereum decentralized at scale.
+        </p>
+        <div
+          className="mt-8 flex w-[220px] items-center justify-center pt-10 "
+          style={{ margin: 'auto' }}>
+          {isConnected ? <DonateDialog /> : <w3m-button label="Donate now" />}
         </div>
-    )
+      </div>
+    </div>
+  )
 }
--- src/components/home/WhySmooth.tsx
+++ src/components/home/WhySmooth.tsx
@@ -1,84 +1,105 @@
 /* eslint-disable jsx-a11y/no-redundant-roles */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 
-import Link from "next/link"
+import Link from 'next/link'
 
 const whySmooth = [
-    {
-        name: '🚀 Boost your MEV earnings:',
-        description:
-            'Pool rewards with other stakers and share profits from every block.',
-    },
-    {
-        name: '🎰 No luck involved:',
-        description: 'Earn an average of all pool rewards, not just your individual blocks.',
-    },
-    {
-        name: '🔍 Full transparency:',
-        description: 'Track all blocks, MEV, and validator performance on the dashboard.',
-    },
-    {
-        name: '✅ Get started easily:',
-        description: 'Just update your fee recipient and claim rewards after proposals. Simple and secure.',
-    },
+  {
+    name: '🚀 Boost your MEV earnings:',
+    description:
+      'Pool rewards with other stakers and share profits from every block.',
+  },
+  {
+    name: '🎰 No luck involved:',
+    description:
+      'Earn an average of all pool rewards, not just your individual blocks.',
+  },
+  {
+    name: '🔍 Full transparency:',
+    description:
+      'Track all blocks, MEV, and validator performance on the dashboard.',
+  },
+  {
+    name: '✅ Get started easily:',
+    description:
+      'Just update your fee recipient and claim rewards after proposals. Simple and secure.',
+  },
 ]
 export default function WhySmooth() {
-    return (
-        <div className="py-24 sm:py-32">
-            <div className="relative">
-                <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                    <div className="mx-auto flex max-w-2xl flex-col gap-16 rounded-2xl bg-gray-400/5 px-6 py-16 shadow sm:p-8 lg:mx-0 lg:max-w-none lg:flex-row lg:items-center lg:py-20 xl:gap-x-20 xl:px-20">
-                        <div style={{ width: '100%', height: '450px', overflow: 'hidden', display: 'flex', justifyContent: 'center', alignItems: 'center', borderRadius: '10px', }}>
-                            <div
-                                style={{
-                                    width: '100%',
-                                    height: '100%',
-                                    background: `url('/images/smooth_poolparty.png')`,
-                                    backgroundSize: 'cover',
-                                    backgroundPosition: 'center',
-                                    transform: 'scale(1.5)',
-                                }}
-                            />
-                        </div>
-                        <div className="w-full flex-auto">
-                            <h2 className="text-3xl font-bold tracking-tight text-DAppDeep dark:text-DAppDarkText sm:text-4xl">Why <span className="text-purple-600">Smooth</span>?</h2>
-                            <p className="mt-6 text-lg leading-8 text-DAppDeep dark:text-DAppDarkText ">
-                                Get higher MEV rewards by joining forces with other stakers.
-                            </p>
-                            <ul
-                                className="mt-10 grid grid-cols-1 gap-x-8 gap-y-3 text-base leading-7 text-DAppDeep dark:text-DAppDarkText sm:grid-cols-2"
-                                role="list"
-                            >
-                                {whySmooth.map((feature) => (
-                                    <li key={feature.name} className="flex cursor-pointer gap-x-3 rounded-md p-2 transition duration-300 hover:scale-110 hover:text-purple-600">
-                                        <p className="text-sm">
-                                            <strong className="text-base">{feature.name}</strong>{' '}
-                                            {feature.description}
-                                        </p>
-                                    </li>
-                                ))}
-                            </ul>
-                            <div className="mt-10 flex">
-                                <Link className="text-sm font-semibold leading-6 text-purple-600" href="/dashboard">
-                                    Go to dashboard <span aria-hidden="true">&rarr;</span>
-                                </Link>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div
-                    aria-hidden="true"
-                    className="absolute inset-x-0 -top-16 -z-10 flex transform-gpu justify-center overflow-hidden blur-3xl"
-                >
-                    <div
-                        className="aspect-[1318/752] w-[82.375rem] flex-none bg-gradient-to-r from-[#80caff] to-[#4f46e5] opacity-25"
-                        style={{
-                            clipPath:
-                                'polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)',
-                        }}
-                    />
-                </div>
+  return (
+    <div className="py-24 sm:py-32">
+      <div className="relative">
+        <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+          <div className="mx-auto flex max-w-2xl flex-col gap-16 rounded-2xl bg-gray-400/5 px-6 py-16 shadow sm:p-8 lg:mx-0 lg:max-w-none lg:flex-row lg:items-center lg:py-20 xl:gap-x-20 xl:px-20">
+            <div
+              style={{
+                width: '100%',
+                height: '450px',
+                overflow: 'hidden',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                borderRadius: '10px',
+              }}>
+              <div
+                style={{
+                  width: '100%',
+                  height: '100%',
+                  background: `url('/images/smooth_poolparty.png')`,
+                  backgroundSize: 'cover',
+                  backgroundPosition: 'center',
+                  transform: 'scale(1.5)',
+                }}
+              />
             </div>
+            <div className="w-full flex-auto">
+              <h2 className="text-3xl font-bold tracking-tight text-DAppDeep dark:text-DAppDarkText sm:text-4xl">
+                Why <span className="text-purple-600">Smooth</span>?
+              </h2>
+              <p className="mt-6 text-lg leading-8 text-DAppDeep dark:text-DAppDarkText ">
+                Get higher MEV rewards by joining forces with other stakers.
+              </p>
+              <ul
+                className="mt-10 grid grid-cols-1 gap-x-8 gap-y-3 text-base leading-7 text-DAppDeep dark:text-DAppDarkText sm:grid-cols-2"
+                role="list">
+                {whySmooth.map((feature) => (
+                  <li
+                    key={feature.name}
+                    className="flex cursor-default gap-x-3 rounded-md p-2 transition duration-300 hover:scale-110 hover:text-purple-600">
+                    <p className="text-sm">
+                      <strong className="text-base">{feature.name}</strong>{' '}
+                      {feature.description}
+                    </p>
+                  </li>
+                ))}
+              </ul>
+              <div className="mt-10 flex">
+                <Link
+                  className="group text-sm font-semibold leading-6 text-purple-600"
+                  href="/dashboard">
+                  Go to dashboard{' '}
+                  <span
+                    aria-hidden="true"
+                    className="inline-block  text-2xl  transition group-hover:translate-x-2">
+                    &rarr;
+                  </span>
+                </Link>
+              </div>
+            </div>
+          </div>
         </div>
-    )
+        <div
+          aria-hidden="true"
+          className="absolute inset-x-0 -top-16 -z-10 flex transform-gpu justify-center overflow-hidden blur-3xl">
+          <div
+            className="aspect-[1318/752] w-[82.375rem] flex-none bg-gradient-to-r from-[#80caff] to-[#4f46e5] opacity-25"
+            style={{
+              clipPath:
+                'polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)',
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
