c8d08913cc16fd67d5f7a5c17617f368e7fe567d
Marc Font <36164126+Marketen@users.noreply.github.com>
2024-07-19 17:29:45 +0200

    unsub simplification (#154)

--- src/components/dialogs/UnsubscribeToMevDialog/dialogs/SuccessDialog.tsx
+++ src/components/dialogs/UnsubscribeToMevDialog/dialogs/SuccessDialog.tsx
@@ -1,21 +1,14 @@
 import { DialogProps } from '../types'
-import { useAccount } from 'wagmi'
-import { useQuery } from '@tanstack/react-query'
 import { StepProgressBar } from '@/components/common/StepProgressBar'
 import { Button } from '@/components/common/Button'
-import { fetchOnChainProof } from '@/client/api/queryFunctions'
 import { CongratulationsIcon } from '@/components/icons'
-import { weiToEth } from '@/utils/web3'
-import { toFixedNoTrailingZeros } from '@/utils/decimals'
 
-export function SuccessDialog({ steps, handleClose }: DialogProps) {
-  const { address } = useAccount()
-  const { data, isLoading, isError } = useQuery({
-    queryKey: ['onchain-proof', address],
-    queryFn: () => fetchOnChainProof(address as `0x${string}`),
-    enabled: !!address,
-  })
+interface SuccessUnsubscribeDialog extends DialogProps {
+  validatorId: number
+}
 
+export function SuccessDialog({ steps, validatorId, handleClose }: SuccessUnsubscribeDialog) {
+  
   return (
     <>
       <div className="-mt-2 text-DAppDeep dark:text-DAppDarkText">
@@ -24,31 +17,17 @@ export function SuccessDialog({ steps, handleClose }: DialogProps) {
       </div>
       <div className="mx-auto flex flex-col items-center gap-y-4 text-center text-lg sm:px-4 text-DAppDeep dark:text-DAppDarkText">
         <CongratulationsIcon />
-        <h4 className="font-bold">Congratulations!</h4>
+        <h4 className="font-bold">Success!</h4>
         <p>
-          You have successfully unsubscribed and withdrawn all your claimable
-          rewards of
-        </p>
-        <p className="font-bold">
-          {isLoading || isError ? (
-            <div className="h-8 w-24 animate-pulse rounded bg-SkeletonGray dark:bg-DAppDarkSurface/300" />
-          ) : (
-            <p className="h-8">
-              {toFixedNoTrailingZeros(
-                weiToEth(data?.claimableRewardsWei || 0),
-                4
-              )}{' '}
-              ETH
-            </p>
-          )}
+          You have successfully unsubscribed validator <b>{validatorId}</b> from Smooth.
         </p>
         <p>
-          You can now change your fee recipient in your validator software
+          You can now change your fee recipient of you validator
           without any penalties. We&#39;re sad to see you go!
         </p>
       </div>
       <Button buttonType="secondary" className="mt-4" onPress={handleClose}>
-        Done
+        Close
       </Button>
     </>
   )
--- src/components/dialogs/UnsubscribeToMevDialog/index.tsx
+++ src/components/dialogs/UnsubscribeToMevDialog/index.tsx
@@ -69,6 +69,7 @@ export function UnsubscribeToMevDialog({
               handleChangeDialogState={setDialogState}
               handleClose={handleCloseDialog}
               steps={steps}
+              validatorId={validatorId}
             />
           )}
         </div>
