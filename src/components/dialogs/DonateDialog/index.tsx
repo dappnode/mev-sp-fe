a27f9ef57ed8e773bc6ea8dd5061ea081e914de6
Marc Font <36164126+Marketen@users.noreply.github.com>
2024-08-26 15:32:16 +0200

    Disable donation button if wallet is not connected or wrong network (#166)
    disable donate now button if wrong network

--- src/components/dialogs/DonateDialog/index.tsx
+++ src/components/dialogs/DonateDialog/index.tsx
@@ -5,6 +5,8 @@ import {
   InitialDialog,
 } from './dialogs'
 import { useState } from 'react'
+import { useAccount } from 'wagmi'
+import { isWalletConnectedChainOk } from '@/utils/web3'
 import { useDialog } from '@/hooks/useDialog'
 import { BaseDialog } from '@/components/dialogs/BaseDialog'
 import type { IDialogStates } from './types'
@@ -12,6 +14,7 @@ import type { IDialogStates } from './types'
 export function DonateDialog() {
   const { open, handleClose, handleOpenChange } = useDialog()
   const [dialogState, setDialogState] = useState<IDialogStates>('initial')
+  const { chain } = useAccount()
 
   const handleCloseDialog = () => {
     handleClose()
@@ -28,6 +31,7 @@ export function DonateDialog() {
       handleOpenChange={handleOpenChangeDialog}
       open={open}
       subtitle="Donate to Smooth"
+      disabledTrigger={!isWalletConnectedChainOk(chain)}
       triggerText="Donate Now">
       {dialogState === 'initial' ? (
         <InitialDialog
